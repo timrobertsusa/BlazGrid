ddee59f98ed8d28b2d58a99f9718851c7782c7f2
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-01-22 02:24:10 +0000

    Add .vscode configuration files and bfake project files

---
+++ bfake/Program.cs
@@ -0,0 +1,41 @@
+﻿using Bogus;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var now = DateTime.Now;
+        Console.WriteLine($"{now}");
+
+        var faker = new Faker();
+        Console.WriteLine(faker.Date.Future(20));
+        Console.WriteLine(faker.Date.Past(22, DateTime.Now));
+        Console.WriteLine(faker.Date.Past(22));
+
+        Console.WriteLine(faker.Date.Past(50));
+        Console.WriteLine(faker.Date.Between(new DateTime(2022, 1, 1), new DateTime(2025, 1, 1)));
+
+        List<PersonClass> people;
+        DateTime startDate = new DateTime(1995, 06, 26);
+        DateTime endDate = DateTime.Now;
+
+        var testUsers = new Bogus.Faker<PersonClass>()
+            .RuleFor(u => u.Id, f => f.Random.Int(1, 100))
+            .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+            .RuleFor(u => u.LastName, f => f.Name.LastName())
+            // .RuleFor(u => u.StartDate, f => f.Date.Between(startDate, endDate))
+            .RuleFor(u => u.StartDate, f => f.Date.Past(22, DateTime.Now))
+            .Generate(5);
+
+        people = testUsers;
+    }
+}
+
+record PersonRecord(int Id, string FirstName, string LastName, DateOnly StartDate);
+public class PersonClass
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime StartDate { get; set; }
+}
